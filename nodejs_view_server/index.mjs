6b7584ae57d6af96f4af48ea4f34473d778824bc
Murilo Preto <murilopreto2003@Outlook.com>
2023-12-19 10:45:40 -0300

    base nodejs server upload

---
+++ nodejs_view_server/index.mjs
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.use(express.json());
+
+app.get("/", function (req, res) {
+    res.render("pages/index");
+  });
+
+app.listen(port, () => {
+  console.log(`Ouvindo na porta ${port}`)
+})
