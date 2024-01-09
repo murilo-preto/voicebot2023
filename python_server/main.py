3157c0d50833c4bda3affe9f2349a32acdea1a1c
Murilo Preto <murilopreto2003@Outlook.com>
2024-01-09 16:47:01 -0300

    login post request

--- python_server/main.py
+++ python_server/main.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+# Teste para GET
 @app.route('/api/pi')
 def get_pi():
-    return {'pi': 3.14159}
+    return {'usuario': 'murilo'}
+
+# Rota post para lidar com Login
+@app.route('/api/login', methods=['POST'])
+def login():
+    uname = request.form.get('uname')
+    psw = request.form.get('psw')
+
+    print(f'{uname}:{psw}')
+
+    return "Success", 202
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
