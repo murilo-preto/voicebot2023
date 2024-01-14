cadeddfa0dc9c5b8afa4afd4b70dff44551a72f6
Murilo Preto <murilopreto2003@Outlook.com>
2024-01-14 17:00:02 -0300

    AUTH
    add partial authentication to python and NodeJS server

--- nodejs_view_server/index.mjs
+++ nodejs_view_server/index.mjs
@@ -1,19 +1,75 @@
-import express from 'express';
+import express from "express";
+import axios from "axios";
+import jwt from "jsonwebtoken";
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const secretKey = 'your-secret-key';
+const authenticateJWT = (req, res, next) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).send('Unauthorized. Please provide a valid token.');
+  }
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) {
+      return res.status(403).send('Token verification failed.');
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
+// Example: Protecting a route using the middleware
+app.get('/protected-route', authenticateJWT, (req, res) => {
+  res.send(`Welcome, ${req.user.username}!`);
+});
 
 app.get("/", function (req, res) {
-    res.render("pages/index");
-  });
+  res.render("pages/index");
+});
 
-  app.get("/login", function (req, res) {
-    res.render("pages/login");
+app.get("/login", function (req, res) {
+  res.render("pages/login");
+});
+
+app.post("/login", async (req, res) => {
+  console.log('Received POST request:', req.body);
+
+  // const username = req.body.uname;
+  // const password = req.body.psw;
+
+  const url = 'http://127.0.0.1:5000/api/login';
+  const postData = {
+    uname: req.body.uname,
+    psw: req.body.psw,
+  };
+
+  axios.post(url, postData)
+  .then(response => {
+    // Handle the successful response
+    console.log('Response:', response.data);
+
+    if (response==401){
+      res.status(401).send("Invalid credentials");
+    }
+    else{
+      res.status(200).send("Login sucessfull");
+    }
+  })
+  .catch(error => {
+    // Handle errors
+    console.error('Error:', error.message);
   });
+});
 
 app.listen(port, () => {
-  console.log(`Ouvindo na porta ${port}`)
-})
+  console.log(`Ouvindo na porta ${port}`);
+});
--- python_server/main.py
+++ python_server/main.py
@@ -1,7 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'your-secret-key'
+jwt = JWTManager(app)
 CORS(app)
 
 # Teste para GET
@@ -12,12 +15,16 @@ def get_pi():
 # Rota post para lidar com Login
 @app.route('/api/login', methods=['POST'])
 def login():
-    uname = request.form.get('uname')
-    psw = request.form.get('psw')
+    json_data = request.get_json()
+    uname = json_data.get('uname')
+    psw = json_data.get('psw')
 
     print(f'{uname}:{psw}')
-
-    return "Success", 202
+    if uname!=None: #Credentials OK
+        access_token = create_access_token(identity=uname)
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify(message='Invalid credentials'), 401
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
