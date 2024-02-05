7a5ffbb8c173c780eb41ebeaf20d28317e88e558
Murilo Preto <murilopreto2003@Outlook.com>
2024-02-05 16:56:38 -0300

    improve database and python pipeline

--- python_sql/main.py
+++ python_sql/main.py
@@ -1,6 +1,5 @@
 import mysql.connector
 
-# Configurações de conexão
 config = {
     'user': 'root',
     'password': 'password',
@@ -9,31 +8,45 @@ config = {
     'port': '3306'  # Porta padrão do MySQL
 }
 
-# Estabelecer conexão
-try:
-    connection = mysql.connector.connect(**config)
+def inserir_usuario(data):
+    try:
+        # Conexão com o banco de dados
+        conn = mysql.connector.connect(**config)
+        cursor = conn.cursor()
 
-    if connection.is_connected():
-        print('Conexão estabelecida com sucesso!')
-        cursor = connection.cursor()
+        # Inserção de um novo usuário na tabela 'documento'
+        sql = "INSERT INTO documento (cpf, primeiro_nome, ultimo_nome, data_nascimento, cargo) VALUES (%s, %s, %s, %s, %s)"
+        val = (data['cpf'], data['primeiro_nome'], data['ultimo_nome'], data['data_nascimento'], data.get('cargo'))
+        cursor.execute(sql, val)
 
-       # Executar o comando SHOW TABLES
-        cursor.execute("SHOW TABLES;")
-        
-        # Recuperar resultados
-        tables = cursor.fetchall()
-        
-        # Imprimir as tabelas
-        print("Tabelas no banco de dados:")
-        for table in tables:
-            print(table[0])
+        # Commit da transação
+        conn.commit()
 
-except mysql.connector.Error as e:
-    print(f"Erro ao conectar ao MySQL: {e}")
-
-finally:
-    # Fechar a conexão
-    if 'connection' in locals() and connection.is_connected():
+        # Fechar cursor e conexão
         cursor.close()
-        connection.close()
-        print("Conexão encerrada.")
+        conn.close()
+
+        return True, 'Usuário adicionado com sucesso!'
+    except mysql.connector.Error as e:
+        error_message = f'Erro ao adicionar usuário: {str(e)}'
+        return False, error_message
+    except KeyError as e:
+        error_message = f'Erro nos dados do usuário: {str(e)}'
+        return False, error_message
+    except Exception as e:
+        error_message = f'Erro desconhecido: {str(e)}'
+        return False, error_message
+
+def test_inserir_usuario():
+    # Caso de teste 1: Inserção bem-sucedida de um novo usuário
+    data_teste_1 = {
+        'cpf': '12345678900',
+        'primeiro_nome': 'João',
+        'ultimo_nome': 'Silva',
+        'data_nascimento': '1990-01-01',
+        'cargo': 'Analista'
+    }
+    success, message = inserir_usuario(data_teste_1)
+    print(success, message)
+
+test_inserir_usuario()
