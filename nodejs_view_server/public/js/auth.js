798c6bca991b7af89b32d18c4d4d51e5fd18dd61
Murilo Preto <murilopreto2003@Outlook.com>
2024-02-27 18:57:06 -0300

    bugfix script not loading

--- nodejs_view_server/public/js/auth.js
+++ nodejs_view_server/public/js/auth.js
@@ -2,10 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const accessToken = localStorage.getItem('accessToken');
 
     if (accessToken) {
-        const headers = {
-            'Authorization': `Bearer ${accessToken}`,
-        };
-
         const tokenPayload = JSON.parse(atob(accessToken.split('.')[1]));
         const username = tokenPayload.sub;
 
@@ -18,14 +14,9 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         const voicebotLink = document.querySelector('.voicebot-link');
-
         if (voicebotLink) {
-            voicebotLink.addEventListener('click', function (event) {
-                event.preventDefault();
-                handleVoicebotLinkClick('/voicebot', headers);
-            });
+            voicebotLink.addEventListener('click', handleVoicebotLinkClick.bind(null, '/voicebot', accessToken));
         }
-
     } else {
         console.log('User is not logged in');
     }
@@ -34,12 +25,8 @@ document.addEventListener('DOMContentLoaded', function () {
 function appendUsernameAndLogoutButton(username, navbar) {
     const usernameContainer = createContainer('span', `Bem vindo, ${username}`, ['navbar-text', 'mr-2']);
     const logoutButton = createContainer('button', 'Desconectar', ['btn', 'btn-outline-secondary']);
-    
-    logoutButton.addEventListener('click', function () {
-        localStorage.removeItem('accessToken');
-        console.log('User logged out');
-        window.location.href = '/login';
-    });
+
+    logoutButton.addEventListener('click', handleLogout);
 
     navbar.appendChild(usernameContainer);
     navbar.appendChild(logoutButton);
@@ -52,17 +39,27 @@ function createContainer(elementType, textContent, classes) {
     return container;
 }
 
-function handleVoicebotLinkClick(url, headers) {
-    fetch(url, {
-        method: 'GET',
-        headers: headers,
-    })
-    .then(response => response.text())
-    .then(html => {
-        const newPage = document.implementation.createHTMLDocument('Voicebot Page');
-        newPage.body.innerHTML = html;
+function handleLogout() {
+    localStorage.removeItem('accessToken');
+    console.log('User logged out');
+    window.location.href = '/login';
+}
 
-        document.body.innerHTML = newPage.documentElement.outerHTML;
-    })
-    .catch(error => console.error('Error:', error));
+function handleVoicebotLinkClick(url, accessToken, event) {
+    event.preventDefault();
+    const headers = {
+        'Authorization': `Bearer ${accessToken}`,
+    };
+    fetch(url, { method: 'GET', headers: headers })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(html => {
+            document.write(html);
+            document.close();
+        })
+        .catch(error => console.error('Error:', error));
 }
