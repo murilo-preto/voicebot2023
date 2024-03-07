dffa7958c16d342ef085d79b80a065c9a08f9f72
Murilo Preto <murilopreto2003@Outlook.com>
2024-03-07 19:03:55 -0300

    rename functions, separate sql functions from test

---
+++ python_server/public/python/sqlTest.py
@@ -0,0 +1,62 @@
+import mysql.connector
+from sqlFunctions import inserir_usuario
+
+config = {
+    'user': 'root',
+    'password': 'password',
+    'host': 'localhost',
+    'database': 'voicebot',
+    'port': '3306'
+}
+
+def test_inserir_usuario():
+    data_teste_1 = {
+        'cpf': '12345678900',
+        'primeiro_nome': 'João',
+        'ultimo_nome': 'Silva',
+        'data_nascimento': '1990-01-01',
+        'cargo': 'paciente'
+    }
+
+    data_teste_2= {
+        'cpf': '34567890155',
+        'primeiro_nome': 'Álvaro',
+        'ultimo_nome': 'Torres',
+        'data_nascimento': '1970-09-25',
+        'cargo': 'medico'
+    }
+
+    success, message = inserir_usuario(data_teste_1)
+    print(success, message)
+
+
+def test_inserir_usuario_interativo():
+    print("Teste de inserção de usuários - Modo Interativo\n")
+
+    while True:
+        print("Por favor, insira os dados do novo usuário:")
+        cpf = input("CPF: ")
+        primeiro_nome = input("Primeiro Nome: ")
+        ultimo_nome = input("Último Nome: ")
+        data_nascimento = input("Data de Nascimento (AAAA-MM-DD): ")
+        cargo = input("Cargo: ")
+
+        novo_usuario = {
+            'cpf': cpf,
+            'primeiro_nome': primeiro_nome,
+            'ultimo_nome': ultimo_nome,
+            'data_nascimento': data_nascimento,
+            'cargo': cargo
+        }
+
+        success, message = inserir_usuario(novo_usuario)
+
+        if success:
+            print(f"\nUsuário adicionado com sucesso: {message}\n")
+        else:
+            print(f"\nFalha ao adicionar usuário: {message}\n")
+
+        opcao = input("Deseja inserir outro usuário? (s/n): ")
+        if opcao.lower() != 's':
+            print("Encerrando o teste de inserção de usuários.")
+            break
